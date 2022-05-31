5e0486d3a5c8d0dacf90f27a59cb7d211ca28467
Ivan Sala <ivaan.sala.m@gmail.com>
2022-05-31 03:42:16 -0300

    inicio

---
+++ clase-1/app.js
@@ -0,0 +1,45 @@
+console.log("hola mundo");
+//palabra asignamos osea var 
+//reservada name var name declara la variable name
+//reservada nombre de valor "Ivan" tipo de valor cuando llamemos la asignador de valor
+
+var name = "Ivan";
+var apellido;
+
+console.log()
+
+//es-6 let y const
+//concatenar valores (unir valores)
+//const nacimiento = 39107482
+
+let name = "jorge"
+let apellido = "gonzales"
+console.log (apellido)
+
+//tipos de valores
+//string= cadena -> todo valor en este formato debe escribirse entre comillas dos simples o invertidas
+
+//Numbres->numeros todos sus valores se escriben sin comillas
+
+//boolean-->valores logicos-->true o false 
+
+// simbol--> = (tipo de valor)
+
+//undefined--> existen pero no tienen un tipo
+
+// regla de escritura camel case aunque la Prime letra siempre va en minuscula , no se puede empezar con numeros pero si con guion bajo o signo pesos entre palabras
+
+let fechaDeNacimiento
+
+let n1 = 30;
+let n2 = 20;
+let result = n1 + n2;
+let resu2 
+
+console.log(result)
+
+//alert solo se puede aceptar ej debe ir dentro de una variable(let) alert ("soy una alerta")
+
+alert ("soy una alerta")
+
+let saludo = alert("hola mundo")
