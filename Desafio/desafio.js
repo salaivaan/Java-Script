0da4f5b0e91b70b2d4ede9dc2d5cdd2b57a4e321
Ivan Sala <ivaan.sala.m@gmail.com>
2022-05-31 04:52:41 -0300

    ejercicios

---
+++ Desafio/desafio.js
@@ -0,0 +1,29 @@
+let nombre = "Maria Belen";
+let apellido= "Mendez";
+let edad= 26;
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+
+const ciudadUno= "Lomas de Zamora";
+const ciudadDos= "Lanus";
+const ciudadTres= "Capital";
+const ciudadCuatro= "Olavarria";
+const ciudadCinco= "Trelew";
+
+console.log (ciudadUno);
+console.log (ciudadDos);
+console.log (ciudadTres);
+console.log (ciudadCuatro);
+console.log (ciudadCinco);
+
+
+const fechaDeNacimiento= 24;
+const mesDeNacimiento= 06;
+const anioDeNacimiento= 1996;
+const espacio=" ";
+let dni=39107459;
+let nacionalidad= "argentina";
+let cedula= nombre + espacio + apellido + espacio + dni + espacio + fechaDeNacimiento + espacio + mesDeNacimiento + espacio +anioDeNacimiento + espacio + ciudadUno + espacio + nacionalidad;
+console.log(cedula)
